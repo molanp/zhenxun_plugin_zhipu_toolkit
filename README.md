9fa726ad4f053f0c67e175fa574d31011785dc9b
molanp <104612722+molanp@users.noreply.github.com>
2025-01-25 17:18:50 +0800

    docs(README): 更新智谱AI插件说明
    - 添加获取API KEY的步骤和使用说明
    - 列出插件功能和使用方法
    - 增加配置说明和依赖安装提示

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # zhenxun_plugin_zhipu_toolkit
-智谱AI全家桶插件，包含智谱平台多种功能
+智谱AI全家桶，省时省力省心
+
+一次安装，多种功能
+
+> ![IMPORTANT]
+> 插件需要智谱AI的API KEY，请先在插件配置中设置
+> 插件使用平台的免费模型，只需注册即可，无任何花费
+
+## 🔑 获取智谱AI的token
+
+前往[https://bigmodel.cn/login](https://bigmodel.cn/login)手机号注册/登录，注册后不需要实名
+
+注册/登录后，访问[此链接](https://bigmodel.cn/usercenter/proj-mgmt/apikeys)即可看到你的`API KEY`,复制后填写即可
+
+**温馨提示:**把鼠标移至TOKEN上，会出现复制按钮，点击即可
+![image](https://github.com/user-attachments/assets/949de9e7-07c8-4451-9d22-a0fd3d5190a9)
+
+## ✨ 功能
+- [x] AI文生图
+- [x] AI文生视频
+- [ ] AI连续对话
+
+## 🚀 安装
+对 Bot 发送`添加插件 zhipu_toolkit`即可安装
+
+## 🎉 使用
+| 命令 | 参数 | 范围 | 说明 |
+|:---:|:---:|:---:|:---:|
+| `生成图片` | `prompt` | 私聊/群聊 | 根据给出文本生成图片,如果没有传入会询问用户 |
+| `生成视频` | `prompt` | 私聊/群聊 | 根据给出文本生成视频,如果没有传入会询问用户 |
+
+## ⚙️ 配置
+
+| 配置项 | 必填 | 默认值 | 说明 |
+|:-----:|:----:|:----:|:----:|
+| `API_KEY` | **是** | `None` | 智谱ai的API KEY |
+
+## 📚 插件依赖
+如果插件报错了没有加载，说明真寻自动安装依赖失败了，请在Bot目录执行以下命令
+```shell
+poetry add zhipuai
+```
---
+++ zhipu_toolkit/__init__.py
@@ -0,0 +1,120 @@
+from zhipuai import ZhipuAI
+from zhenxun.configs.utils import PluginExtraData, RegisterConfig
+from zhenxun.configs.config import Config
+from arclet.alconna import Args, Alconna
+from nonebot.plugin import PluginMetadata
+from nonebot import require
+import asyncio
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import on_alconna, Match, Text, Image, Video  # noqa: E402
+
+
+__plugin_meta__ = PluginMetadata(
+    name="AI全家桶",
+    description="AI全家桶，省时省力省心",
+    usage="""
+    AI全家桶，省时省力省心
+    usage:
+        生成图片 <prompt> 
+        生成视频 <prompt>
+    """.strip(),
+    extra=PluginExtraData(
+        author="molanp",
+        version="0.1",
+        menu_type="群内小游戏",
+        configs=[
+            RegisterConfig(
+                key="API_KEY",
+                value=None,
+                help="智谱AI平台的APIKEY",
+                default_value=None,
+            )
+        ],
+    ).dict(),
+)
+
+api_key = Config.get_config("zhipu_toolkit", "API_KEY", None)
+if api_key is None:
+    raise ValueError("请先设置智谱AI的APIKEY")
+
+client = ZhipuAI(api_key=api_key)
+
+draw = on_alconna(
+    Alconna("生成图片", Args["msg?", str, "all"]),
+    priority=5,
+    block=True
+)
+
+draw_video = on_alconna(
+    Alconna("生成视频", Args["message?", str, "all"]),
+    priority=5,
+    block=True
+)
+
+@draw.handle()
+async def _(msg: Match[str], size: Match[str]):
+    if msg.available:
+        draw.set_path_arg("msg", msg.result)
+
+@draw_video.handle()
+async def _(message: Match[str], size: Match[str]):
+    if message.available:
+        draw.set_path_arg("message", message.result)
+
+@draw.got_path("msg", prompt="你要画什么呢")
+async def handle_check(msg: str):
+    try:
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(
+            None, lambda: client.images.generations(
+                model="cogview-3-flash",
+                prompt=msg,
+                size="1440x720")
+        )
+        await draw.send(Image(url=response.data[0].url), reply_to=True)
+    except Exception as e:
+        await draw.send(Text(f"错了：{e}"))
+
+@draw_video.got_path("message", prompt="你要制作什么视频呢")
+async def submit_task(message: str):
+    try:
+        response = await submit_task_to_zhipuai(message)
+    except Exception as e:
+        await draw_video.send(Text(str(e)))
+    else:
+        if response.task_status != "FAIL":
+            await draw_video.send(Text(f"任务已提交,id: {response.id}"))
+            # 启动异步任务检查状态
+            asyncio.create_task(
+                check_task_status_periodically(response.id, draw_video))
+        else:
+            await draw_video.send(Text(f"任务提交失败，e:{response}"))
+
+
+async def submit_task_to_zhipuai(message: str):
+    return client.videos.generations(
+        model="cogvideox-flash",  # 替换为适当的模型编码
+        prompt=message,
+        with_audio=True,
+    )
+
+
+async def check_task_status_periodically(task_id: str, m):
+    while True:
+        try:
+            response = await check_task_status_from_zhipuai(task_id)
+        except Exception as e:
+            await m.send(Text(str(e)), reply_to=True)
+            break
+        else:
+            if response.task_status == "SUCCESS":
+                await m.send(Video(
+                    url=response.video_result[0].url))
+                break
+            elif response.task_status == "FAIL":
+                await m.send(Text("生成失败了..", reply_to=True))
+            await asyncio.sleep(5)
+
+
+async def check_task_status_from_zhipuai(task_id: str):
+    return client.videos.retrieve_videos_result(id=task_id)
