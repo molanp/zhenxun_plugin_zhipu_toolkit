7cab182e44437f262a4897c4c5a196ee45edd0b6
molanp <104612722+molanp@users.noreply.github.com>
2025-01-25 17:39:23 +0800

    fix(zhipu_toolkit): 修复 APIKEY 未设置时的错误处理
    - 在 handle_check 和 submit_task 函数中增加了对 APIKEY 未设置的判断
    - 当 APIKEY 未设置时，发送提示消息并提前终止函数执行
    - 优化了错误处理结构，提高了代码的可读性和健壮性

--- zhipu_toolkit/__init__.py
+++ zhipu_toolkit/__init__.py
@@ -64,32 +64,36 @@ async def _(message: Match[str]):
 async def handle_check(msg: str):
     if api_key is None:
         await draw_pic.send(Text("请先设置智谱AI的APIKEY!"), reply_to=True)
-    try:
-        loop = asyncio.get_event_loop()
-        response = await loop.run_in_executor(
-            None, lambda: client.images.generations(
-                model="cogview-3-flash",
-                prompt=msg,
-                size="1440x720")
-        )
-        await draw_pic.send(Image(url=response.data[0].url), reply_to=True)
-    except Exception as e:
-        await draw_pic.send(Text(f"错了：{e}"), reply_to=True)
+    else:
+        try:
+            loop = asyncio.get_event_loop()
+            response = await loop.run_in_executor(
+                None, lambda: client.images.generations(
+                    model="cogview-3-flash",
+                    prompt=msg,
+                    size="1440x720")
+            )
+            await draw_pic.send(Image(url=response.data[0].url), reply_to=True)
+        except Exception as e:
+            await draw_pic.send(Text(f"错了：{e}"), reply_to=True)
 
 @draw_video.got_path("message", prompt="你要制作什么视频呢")
 async def submit_task(message: str):
-    try:
-        response = await submit_task_to_zhipuai(message)
-    except Exception as e:
-        await draw_video.send(Text(str(e)))
+    if api_key is None:
+        await draw_pic.send(Text("请先设置智谱AI的APIKEY!"), reply_to=True)
     else:
-        if response.task_status != "FAIL":
-            await draw_video.send(Text(f"任务已提交,id: {response.id}"), reply_to=True)
-            # 启动异步任务检查状态
-            asyncio.create_task(
-                check_task_status_periodically(response.id, draw_video))
+        try:
+            response = await submit_task_to_zhipuai(message)
+        except Exception as e:
+            await draw_video.send(Text(str(e)))
         else:
-            await draw_video.send(Text(f"任务提交失败，e:{response}"), reply_to=True)
+            if response.task_status != "FAIL":
+                await draw_video.send(Text(f"任务已提交,id: {response.id}"), reply_to=True)
+                # 启动异步任务检查状态
+                asyncio.create_task(
+                    check_task_status_periodically(response.id, draw_video))
+            else:
+                await draw_video.send(Text(f"任务提交失败，e:{response}"), reply_to=True)
 
 
 async def submit_task_to_zhipuai(message: str):
