b11835881890bd4b951ed725bb8ae1f7c4e115d7
molanp <104612722+molanp@users.noreply.github.com>
2025-02-11 20:09:57 +0800

    build(zhipu_toolkit): 更新版本号至0.6
    - 在 __init__.py 文件中将插件版本号从 0.5 修改为 0.6
    - 此次更新标志着群内小游戏功能的进一步完善

--- zhipu_toolkit/__init__.py
+++ zhipu_toolkit/__init__.py
@@ -31,7 +31,7 @@ __plugin_meta__ = PluginMetadata(
     """.strip(),
     extra=PluginExtraData(
         author="molanp",
-        version="0.5",
+        version="0.6",
         menu_type="群内小游戏",
         superuser_help="""
         超级管理员额外命令
