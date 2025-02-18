316f2ee55c8b46ef73b0450757b593ddf2310fa3
molanp <molanp>
2025-02-18 22:22:46 +0800

    update __init__.py

--- zhipu_toolkit/__init__.py
+++ zhipu_toolkit/__init__.py
@@ -31,7 +31,7 @@ __plugin_meta__ = PluginMetadata(
     """.strip(),
     extra=PluginExtraData(
         author="molanp",
-        version="0.7-rc1",
+        version="0.7-rc2",
         menu_type="群内小游戏",
         superuser_help="""
         超级管理员额外命令
