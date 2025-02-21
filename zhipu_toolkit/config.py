25075ab55d6cf04fb8026cfe93b9579453359a5e
molanp <104612722+molanp@users.noreply.github.com>
2025-02-21 20:06:32 +0800

    refactor(zhipu_toolkit): 移除 ChatConfig 中的默认配置
    - 删除了 ChatConfig 类中的 default 字典，减少了冗余代码
    - 简化了 get 方法，提高了代码可读性

--- zhipu_toolkit/config.py
+++ zhipu_toolkit/config.py
@@ -1,5 +1,3 @@
-from typing import ClassVar
-
 import nonebot
 from pydantic import BaseModel, Extra
 
@@ -7,25 +5,10 @@ from zhenxun.configs.config import Config
 
 
 class ChatConfig:
-    default: ClassVar[dict] = {
-        "API_KEY": "",
-        "CHAT_MODEL": "glm-4-flash",
-        "PIC_MODEL": "cogview-3-flash",
-        "VIDEO_MODEL": "cogvideox-flash",
-        "IMAGE_UNDERSTANDING_MODEL": "glm-4v-flash",
-        "SOUL": "你是真寻，你强大且无所不能",
-        "CHAT_MODE": "user",
-        "IMPERSONATION_MODE": False,
-        "IMPERSONATION_TRIGGER_FREQUENCY": 20,
-        "IMPERSONATION_MODEL": "glm-4-flash",
-        "IMPERSONATION_SOUL": False,
-        "IMPERSONATION_BAN_GROUP": []
-    }
-
     @classmethod
     def get(cls, key: str):
         key = key.upper()
-        return Config.get_config("zhipu_toolkit", key, cls.default.get(key))
+        return Config.get_config("zhipu_toolkit", key)
 
 
 class PluginConfig(BaseModel, extra=Extra.ignore):
