39deceb3aad7250fa89a2178a41d3cc6435b7b74
molanp <104612722+molanp@users.noreply.github.com>
2025-03-01 17:26:24 +0800

    refactor(zhipu_toolkit): 优化伪人提示词
    - 将插件版本号从 0.8-rc1 升级到 0.8-rc2
    - 优化了 ChatManager 类中生成聊天记录头部的逻辑，明确了回应消息的格式要求

--- zhipu_toolkit/data_source.py
+++ zhipu_toolkit/data_source.py
@@ -303,7 +303,7 @@ class ChatManager:
         bot = get_bot(self_id=session.self_id)
         my_info = await bot.get_group_member_info(group_id=gid, user_id=session.self_id)
         my_name = my_info["card"] or my_info["nickname"]
-        head = f"你在一个QQ群里，请你以`{my_name}({session.self_id})`的身份加入聊天，做出一条格式标准的回应，不允许回应自己的消息，需要参与讨论，混入其中。如果觉得此时不需要自己说话，请只回复`<EMPTY>`。下面是群组的聊天记录：\n```"  # noqa: E501
+        head = f"你在一个QQ群里，请你以`{my_name}({session.self_id})`的身份加入聊天，做出一条符合格式`[发送于 YY-mm-dd H:M:S from {{username}}]:{{message}}`的回应，不允许回应自己的消息，需要参与讨论，混入其中。如果觉得此时不需要自己说话，请只回复`<EMPTY>`。下面是群组的聊天记录：\n```"  # noqa: E501
         foot = (
             "\n```"
         )
