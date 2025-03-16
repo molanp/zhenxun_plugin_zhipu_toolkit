4e5aaaaf9dd577d47a56ddf1496d09a92441e5f2
molanp <molanp>
2025-03-16 08:59:07 +0800

    修正回复格式

--- zhipu_toolkit/data_source.py
+++ zhipu_toolkit/data_source.py
@@ -321,7 +321,7 @@ class ChatManager:
             f"[{msg.time} NICKNAME {msg.nickname} UID {msg.uid}]:{msg.msg}\n\n"
             for msg in group_msg
         )
-        head = f"当前时间为{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n**\n你在一个QQ群里，请你参与讨论，只能以`{BotConfig.self_nickname}({session.self_id})`的身份发言一次，不允许多次重复一样的话，不允许回应自己的消息.如果觉得此时不需要自己说话，请只回复`<EMPTY>`。\n*** 回复格式为`[username(uin)]:message`***\n下面是群组的聊天记录：\n***"  # noqa: E501
+        head = f"当前时间为{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n**\n你在一个QQ群里，请你参与讨论，只能以`{BotConfig.self_nickname}({session.self_id})`的身份发言一次，不允许多次重复一样的话，不允许回应自己的消息.如果觉得此时不需要自己说话，请只回复`<EMPTY>`。\n*** 回复格式为`username(uin):message`***\n下面是群组的聊天记录：\n***"  # noqa: E501
         foot = "\n***"
         soul = (
             ChatConfig.get("SOUL")
